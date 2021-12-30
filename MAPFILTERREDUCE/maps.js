03f6e9d519f95a19c59a0b6784a0a219721a7714
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2021-12-30 15:26:19 -0500

    add: types of maps JS

--- MAPFILTERREDUCE/maps.js
+++ MAPFILTERREDUCE/maps.js
@@ -2,6 +2,23 @@ const dollars = ["32$", "15$", "12$", "17$", "20$"];
 
 // Map
 
+// Forma boomer
+let prices = [];
+for (let i = 0; i < dollars.length; i++) {
+  prices[i] = Number(dollars[i].slice(0, dollars[i].length - 1));
+}
+
+// Forma zoomer
+prices = [];
+for (const dollar of dollars) {
+  prices.push(Number(dollar.slice(0, dollar.length - 1)));
+}
+
+// Forma atualizada
+prices = dollars.map((dollar) => Number(dollar.slice(0, dollar.length - 1)));
+
+console.log(prices);
+
 // Filter
 
 // Reduce
