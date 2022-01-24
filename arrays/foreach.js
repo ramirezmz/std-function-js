6d12df7ca414174e7b52b19d2ab4b7d9e6d5bc5e
ramirezmz <roberto.paolo@dremio.com>
2022-01-24 16:34:37 -0300

    add forEach+arrow Function

--- arrays/foreach.js
+++ arrays/foreach.js
@@ -3,3 +3,9 @@ const aprovados = ["Amanda", "Roberto", "Pedrinho", "Thor"];
 aprovados.forEach(function (nome, indice) {
   console.log(`${indice + 1}) ${nome}`);
 });
+console.log("======================");
+aprovados.forEach((nome, indice) => console.log(`${indice + 1}) ${nome}`));
+
+console.log("======================");
+const exibirAprovados = (aprovados) => console.log(aprovados);
+aprovados.forEach(exibirAprovados);
