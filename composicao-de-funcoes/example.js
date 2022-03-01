fe5d7d08afa69f0be414fc2152e600b8e438c050
ramirezmz <roberto.paolo@dremio.com>
2022-03-01 07:25:10 -0300

    fix: refact function compositions

--- composicao-de-funcoes/example.js
+++ composicao-de-funcoes/example.js
@@ -1,21 +1,27 @@
-const gritar = x => x.toUpperCase()
-const exclamar = x => `${x}!!!`
-const perguntar = x => `${x}???`
-
+const gritar = (x) => x.toUpperCase();
+const exclamar = (x) => `${x}!!!`;
+const perguntar = (x) => `${x}???`;
+const esticar = (x) =>
+  x
+    .split("")
+    .reduce((final, letter) => final + `${letter}${letter}${letter}`)
+    .replace(/\s+/g, " ");
 // console.log(exclamar(gritar('paolo')))
 
-const compor = (func1, func2, func3) => {
-   return function(text){
-      let resultado = func1(text)
-      resultado = func2(resultado)
-      resultado = func3(resultado)
-      return resultado
-   }
-}
+const compor = (...funcoes) => {
+  return function (text) {
+    let resultado = text;
+    for (let funcao of funcoes) {
+      resultado = funcao(resultado);
+    }
+    return resultado;
+  };
+};
 // const resultado = compor(gritar, exclamar, 'bom dia')
 // console.log(resultado)
-const exagerar = compor(gritar,perguntar, exclamar)
-const resultado1 = exagerar('bom dia')
-console.log(resultado1)
-const resultado2 = exagerar('boa tarde')
-console.log(resultado2)
+const exagerarMenos = compor(gritar, exclamar);
+const exagerar = compor(gritar, perguntar, exclamar);
+const exagerarMais = compor(gritar, perguntar, exclamar, esticar);
+console.log(exagerarMenos("bom dia"));
+console.log(exagerar("boa tarde"));
+console.log(exagerarMais("boa noite"));
