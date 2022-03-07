da664bc0a40672b7773f0c6e5ef6fc50d2857bd1
ramirezmz <roberto.paolo@dremio.com>
2022-03-07 18:07:42 -0300

    add: methods string

---
+++ metodos-string/index.js
@@ -0,0 +1,5 @@
+const firstName = 'Robertopaolo'
+const lastName = 'Ramirez'
+console.log(firstName.length)
+console.log(firstName.includes('paolo'))// para verificar se o valor esta dentro da variável.
+console.log(firstName.slice(0, 4))
